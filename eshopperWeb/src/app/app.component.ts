1206c89618425868c6615e7f219ac219fcdb5623
sekhwelad <25sekhwelad@gmail.com>
2023-08-05 12:11:11 +0200

    Product Interface

--- eshopperWeb/src/app/app.component.ts
+++ eshopperWeb/src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Product } from './models/product';
+import { Pagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'eShopper Web';
+  products: Product[] = [];
+
+  constructor(private httpClient: HttpClient) {}
+  ngOnInit(): void {
+    this.httpClient
+      .get<Pagination<Product[]>>(
+        'https://localhost:5001/api/products?pageSize=50'
+      )
+      .subscribe({
+        next: (response: any) => {
+          console.log(response);
+          this.products = response.data;
+        },
+        error: (error) => {
+          console.log(error);
+        },
+        complete: () => {
+          console.log('Request Completed');
+        },
+      });
+  }
 }
