9e47f35518527e1a52984235fe2cff91fb35e5cb
sekhwelad <25sekhwelad@gmail.com>
2023-08-06 21:23:14 +0200

    Client error handling

---
+++ eshopperWeb/src/app/core/test-error/test-error.component.ts
@@ -0,0 +1,46 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { environment } from 'src/environments/environments';
+
+@Component({
+  selector: 'app-test-error',
+  templateUrl: './test-error.component.html',
+  styleUrls: ['./test-error.component.scss'],
+})
+export class TestErrorComponent {
+  baseUrl = environment.apiUrl;
+  validationErrors: string[] = [];
+
+  constructor(private httpClient: HttpClient) {}
+
+  get404Error() {
+    this.httpClient.get(this.baseUrl + 'products/42').subscribe({
+      next: (response) => console.log(response),
+      error: (error) => console.log(error),
+    });
+  }
+
+  get500Error() {
+    this.httpClient.get(this.baseUrl + 'buggy/servererror').subscribe({
+      next: (response) => console.log(response),
+      error: (error) => console.log(error),
+    });
+  }
+
+  get400Error() {
+    this.httpClient.get(this.baseUrl + 'buggy/badrequest').subscribe({
+      next: (response) => console.log(response),
+      error: (error) => console.log(error),
+    });
+  }
+
+  get400ValidationError() {
+    this.httpClient.get(this.baseUrl + 'products/fortytwo').subscribe({
+      next: (response) => console.log(response),
+      error: (error) => {
+        console.log(error);
+        this.validationErrors = error.errors;
+      },
+    });
+  }
+}
