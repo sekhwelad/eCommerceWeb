749fd72ac7fb3a680c4f729b55862eb202a9c11a
sekhwelad <25sekhwelad@gmail.com>
2023-08-08 13:25:56 +0200

    Styling the UI

--- eshopperWeb/src/app/home/home.module.ts
+++ eshopperWeb/src/app/home/home.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
-import { RouterModule } from '@angular/router';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, SharedModule],
   exports: [HomeComponent],
 })
 export class HomeModule {}
